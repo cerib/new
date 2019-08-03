ad3a0bc426717ba12399a70292bd3d72e180b26a
cerib <impcesar@gmail.com>
2019-08-03 19:04:20 +0100

    Updates

---
+++ precache-manifest.a32979a50b8a3bd9d783a3f3b7bfd123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95af453f3e5ba8d3a09bf5f2a25ffe19",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "fb5e2bc3ded4428a219e",
+    "url": "/new/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/new/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fb5e2bc3ded4428a219e",
+    "url": "/new/static/js/main.f1d8dd5c.chunk.js"
+  },
+  {
+    "revision": "203f4010d004ffdf2b66",
+    "url": "/new/static/js/runtime~main.dee46077.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new/static/media/logo.5d5d9eef.svg"
+  }
+]);
