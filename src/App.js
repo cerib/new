5f71120dcbfd67601cf59dfd7a9c6a02d6269e57
cerib <impcesar@gmail.com>
2019-08-03 19:18:47 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import WelcomeMessage from "./WelcomeMessage/welcomemessage";
+import TutorsSection from "./Tutors/tutorssection";
 
-function App() {
+import "./homepage.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="background-color-fullwidth">
+        <WelcomeMessage />
+      </div>
+      <div>
+        <TutorsSection />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
